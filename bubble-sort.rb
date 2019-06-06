40946d5fefd2b56c7cdd58d759af96fc68fe3afa
Sumaer Jolly <sumaerjolly@gmail.com>
2019-06-06 15:29:11 +0530

    bubble sort added

---
+++ bubble-sort.rb
@@ -0,0 +1,24 @@
+def bubble_sort(array)
+    n = array.length
+
+    (n-1).times {
+        array.each_index { |index|
+        if array[index+1]
+            current_element = array[index]
+            next_element = array[index+1]
+
+            if current_element > next_element
+                array[index] = next_element
+                array[index+1] = current_element
+            end
+        
+        end    
+        }
+    }
+    print array
+
+end
+
+
+bubble_sort([5,2,1,4,3,6])
+bubble_sort([100,80,30,1000,20,40,60])
